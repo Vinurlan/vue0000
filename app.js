0ff374ea5cb33592185e4dcb1ae28d45ee62f391
Vinurlan <dosug.d@gmail.com>
2020-01-30 01:02:20 +0300

    form to vue

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
+Vue.component('madal-form', {
+    props: ["propsform"],
+    template: `
+        <form id="madalFormCallback" class="madalFormContainer">
+            <madal-input
+                v-for="(prop, index) of propsform"
+                v-bind:key="index"
+                v-bind:propsinput="prop"
+            ></madal-input>
+        </form>
+    `
+})
+
+Vue.component('madal-input', {
+    props: ["propsinput", "propvalue"],
+    template: `
+        <label>
+        
+            <label v-bind:for=propsinput.id >{{propsinput.label}}</label>
+            <input 
+                v-bind:type=propsinput.type
+                v-bind:id=propsinput.id
+                v-bind:v-model=propvalue
+                v-on:keyUp="$emit('handle-valid')"
+                >
+        </label>
+    `
+})
 
 
 var madalView = new Vue({
     el: "#madal-view",
-    data: {
+    data: {   
         madalCallback: false,
         dataCallback: {
             name: "",
@@ -11,6 +39,11 @@ var madalView = new Vue({
         },
         isValid: false,
         madalMessage: false,
+        madalFormInputs: [
+            { label: "Ваше имя*", id: "madalName", type: "text", value: "dataCallback.name", valid: "validHandler"},
+            { label: "Ваш номер телефона*", id: "madalPhone", type: "number", value: "dataCallback.number", valid: "validHandler"},
+            { label: "Комментарий к заявке", id: "madalComment", type: "text", value: "dataCallback.comment", valid: ""},
+        ],
     },
     methods: {
         closeMadal: function() {
@@ -45,8 +78,8 @@ var madalView = new Vue({
             if (!this.isValid) {
                 return;
             }
-            axios.post("https://basebackpack.firebaseio.com/client-request.json", this.dataCallback)
-                .then(response => response.json);
+            /*axios.post("https://basebackpack.firebaseio.com/client-request.json", this.dataCallback)
+                .then(response => response.json);*/
             this.closeMadal();
             this.isValid = false;
             this.madalMessage = true;
